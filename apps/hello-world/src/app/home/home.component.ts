c3d0424c1a9574ebbc45a8596a6d98670b7201cc
Santosh Yadav <s.yadav@celonis.com>
2023-05-20 19:22:02 +0200

    feat: add title

--- apps/hello-world/src/app/home/home.component.ts
+++ apps/hello-world/src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AccordionComponent } from '@conf-demo/design-system';
+import { TitleComponent } from '@conf-demo/ui-state';
 
 @Component({
   selector: 'conf-demo-home',
   standalone: true,
-  imports: [CommonModule, AccordionComponent],
+  imports: [CommonModule, AccordionComponent, TitleComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
