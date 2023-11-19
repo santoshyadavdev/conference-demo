41fb387a873310c992b6aaa7edf6e0d4c2a1bf4e
Santosh Yadav <s.yadav@celonis.com>
2023-11-19 16:39:15 +0100

    demo app

---
+++ libs/ui-lib/src/lib/ui-lib.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {DesignSystemModule } from '@conf-demo/design-system';
+
+
+@NgModule({
+  imports: [CommonModule, DesignSystemModule],
+})
+export class UiLibModule {}
